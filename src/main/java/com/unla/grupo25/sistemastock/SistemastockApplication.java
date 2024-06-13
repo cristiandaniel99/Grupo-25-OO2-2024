9fe62d967f12c92730e91cd1af2f702d5e23cd8f
RodrigoReinoso07 <rodrigoreinoso9@gmail.com>
2024-06-12 22:32:03 -0300

    Agregando login

---
+++ src/main/java/com/unla/grupo25/sistemastock/SistemastockApplication.java
@@ -0,0 +1,13 @@
+package com.unla.grupo25.sistemastock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemastockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemastockApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/unla/grupo25/sistemastock/helpers/ViewRouteHelper.java
@@ -0,0 +1,14 @@
+package com.unla.grupo25.sistemastock.helpers;
+
+public class ViewRouteHelper {
+	
+	// USER HELPERS
+	public static final String USER_LOGIN = "user/login";
+	public static final String USER_LOGOUT = "user/logout";
+	
+	//
+	
+	public static final String INDEX = "home/index";
+	public static final String ROUTE = "/index";
+	
+}
