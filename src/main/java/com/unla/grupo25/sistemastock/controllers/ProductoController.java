c8b59f9f17fd48fb8e08ab2638cab9ab0ba45625
cristian <cristiandaniel99alegre@gmail.com>
2024-06-15 23:57:17 -0300

    Agregando CRUD producto y estilos

---
+++ src/main/java/com/unla/grupo25/sistemastock/controllers/ProductoController.java
@@ -0,0 +1,67 @@
+package com.unla.grupo25.sistemastock.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.unla.grupo25.sistemastock.dtos.ProductoDTO;
+import com.unla.grupo25.sistemastock.dtos.StockDTO;
+import com.unla.grupo25.sistemastock.helpers.ViewRouteHelper;
+import com.unla.grupo25.sistemastock.services.IProductoService;
+
+import com.unla.grupo25.sistemastock.entities.Producto;
+import com.unla.grupo25.sistemastock.services.IProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+
+@Controller
+@RequestMapping("/productos")
+public class ProductoController {
+
+    @Autowired
+    private IProductoService productoService;
+
+    @GetMapping
+    public ModelAndView listarProductos() {
+    	ModelAndView mAV = new ModelAndView(ViewRouteHelper.PRODUCTO_INDEX); 
+		mAV.addObject("productos",productoService.getAll()); 
+		mAV.addObject("producto", new ProductoDTO());
+		return mAV; 
+    }
+
+    @GetMapping("/editar/{id}")
+    public ModelAndView mostrarFormularioEditar(@PathVariable int id) {
+        Optional<Producto> producto = productoService.findById(id);
+        ModelAndView modelAndView = new ModelAndView(ViewRouteHelper.FORM_PRODUCTO); 
+        modelAndView.addObject("producto", producto.orElse(new Producto()));
+        return modelAndView;
+    }
+
+    @PostMapping("/save") 
+	public ModelAndView create(@ModelAttribute("producto") ProductoDTO productoDTO) {
+		productoService.insertOrUpdate(productoDTO); 
+		return new ModelAndView("redirect:/productos");
+	}
+
+    @GetMapping("/eliminar/{id}")
+    public ModelAndView eliminarProducto(@PathVariable int id) {
+        productoService.remove(id);
+        return new ModelAndView("redirect:/productos");
+    }
+}
+
+
+	
+	
+
